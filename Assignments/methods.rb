fa0aeb5ead1e285871798bddb1129ded1b455e34
Anthematics <Jasonharder2@gmail.comX>
2018-08-29 19:48:10 -0400

    nearly finished assignment1 just refactoring (very basic)

--- Assignments/methods.rb
+++ Assignments/methods.rb
@@ -50,11 +50,36 @@ end
 greettwice("Jason")
 
 def farenheittocel(farenheit)
+    cel = (farenheit - 32) * 5/9
+    puts "if it is #{farenheit} F then that would be #{cel} C "
+end
 
-puts "if it is #{far} then that would be #{cel} celsuis "
+farenheittocel(100)
+
+
+def wrap_text(text,symbol)
+
+puts "#{symbol}#{text}#{symbol}"
 
 end
 
-def wrap_text(text)
+wrap_text("I like cheese", "++++")
 
+def distanceran(person,distance,minutes)
+    puts "how far did #{person} run in meters"
 end
+
+
+
+puts "How far did person 1 run (in metres)?"
+distance1 = gets.to_f
+puts "How long (in minutes) did person 1 run take to run #{distance1} metres?"
+mins1 = gets.to_f
+
+
+secs1 = mins1 * 60
+speed1 = distance1/secs1
+secs2 = mins2 * 60
+speed2 = distance2/secs2
+secs3 = mins3 * 60
+speed3 = distance3/secs3
